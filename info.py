a521edb5b3639b1c43616ee394813b802999f06c
jnaneshgowda123 <104252761+jnaneshgowda123@users.noreply.github.com>
2025-09-02 16:24:07 +0600

    Update info.py

--- info.py
+++ info.py
@@ -40,6 +40,9 @@ TRY_AGAIN_BTN = bool(environ.get('TRY_AGAIN_BTN', False)) # Set True Or False (T
 auth_channel = environ.get('AUTH_CHANNEL', '') # give your force subscribe channel id here else leave it blank
 AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else None
 
+# Multiple Force Subscribe Channels - For Unlimited Force Subscribe
+FORCE_SUB_CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('FORCE_SUB_CHANNELS', '').split()]  # For Multiple Id Use One Space Between Each.
+
 # This Channel Is For When User Request Any File Name With command or hashtag like - /request or #request
 reqst_channel = environ.get('REQST_CHANNEL', '')
 REQST_CHANNEL = int(reqst_channel) if reqst_channel and id_pattern.search(reqst_channel) else None
